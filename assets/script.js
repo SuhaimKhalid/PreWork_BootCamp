cee408d1009f581d7e057ef74e276d28edeaac9c
Suhaim Khalid <suhaimkhalid007@gmail.com>
2023-09-23 15:44:13 +0100

    Add Js to Project

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+var shape = "circle";
+var topics = "HTML, CSS, Git, JavaScript";
+console.log(topics);
+var topic = "HTML";
+
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+var a;
+for (a=0; a<shapes.length; a++)
+{
+    console.log(shapes[a]);
+}
